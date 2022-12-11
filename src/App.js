b8d84b3d28a368d9f6f70adcbe7e2c5ce816ab64
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2022-12-11 22:32:14 +0100

    About page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import About from './components/About';
 
 const App=()=>{
   return (
     <div>
       <Navbar/>
       <Home/>
+      <About/>
     </div>
   )
 } 
--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,26 @@ import React from 'react'
 
 const About=()=>{
    return (
-      <div>
-         About
+      <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      <div className='flex flex-col justify-center items-center w-full h-full'>
+        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+          <div className='sm:text-right pb-8 pl-4'>
+            <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
+              About
+            </p>
+          </div>
+          <div></div>
+         </div>
+         <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+            <div className='sm:text-right text-4xl font-bold'>
+              <p>Hi, I'm Mohammad Shafi</p>
+            </div>
+            <div>
+              <p>I am a hard working, honest individual. I am a good timekeeper, always willing to learn new skills. I am friendly, helpful and polite, have a good sense of humour. I am able to work independently in busy environments and also within a team setting. I am outgoing and tactful, and able to listen effectively when solving problems.</p>  
+            </div>
+         </div>
       </div>
+    </div>
    )
 }
 
