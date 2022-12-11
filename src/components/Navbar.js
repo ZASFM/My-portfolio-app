97c43e60f134024c9ef82089c0d69b2d9f0d7307
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2022-12-11 21:26:28 +0100

    Adding social links & menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,13 @@
 import React,{useState} from 'react';
-import {FaTimes,FaBars} from 'react-icons/fa';
+import {FaTimes,FaBars,FaGithub,FaLinkedin} from 'react-icons/fa';
+import {HiOutlineMail} from 'react-icons/hi'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 
 const Navbar=()=>{
    const [nav,setNav]=useState(false);
+   const handleClick=()=>{
+      setNav(preVal=>!preVal);
+   }
    return (
       <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
          <div>
@@ -18,11 +23,11 @@ const Navbar=()=>{
             </ul>
 
          {/*Burger*/}
-         <div className='md:hidden z-10 '>
-            <FaBars/>
+         <div className='md:hidden z-10' onClick={handleClick}>
+            {!nav? <FaBars/>:<FaTimes/>}
          </div>
          {/*mobile menu*/}
-         <ul className='hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
+         <ul className={!nav?'hidden':'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
             <li className='py-6 text-4xl'>Home</li>
             <li className='py-6 text-4xl'>About</li>
             <li className='py-6 text-4xl'>Skills</li>
@@ -30,8 +35,31 @@ const Navbar=()=>{
             <li className='py-6 text-4xl'>Contact</li>
          </ul>
          {/*socials*/}
-         <div className='hidden'></div>
-      </div>
+         <div className='hidden lg:flex fixed top-[35%] left-0 flex-col'>
+            <ul>
+               <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+                  <a target="_blank" rel='noreferrer' className="flex justify-between items-center w-full text-gray-300" href="https://www.linkedin.com/in/mohammad-dawood-nazer-2963a9231/">
+                     LinkedIn<FaLinkedin size={30}/>
+                  </a>
+               </li>
+               <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+                  <a target="_blank" rel='noreferrer' className="flex justify-between items-center w-full text-gray-300" href="https://github.com/ZASFM">
+                     Github<FaGithub size={30}/>
+                  </a>
+               </li>
+               <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+                  <a target="_blank" rel='noreferrer' className="flex justify-between items-center w-full text-gray-300" href="https://gmail.com">
+                     Mail<HiOutlineMail size={30}/>
+                  </a>
+               </li>
+               <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+                  <a target="_blank" rel='noreferrer' className="flex justify-between items-center w-full text-gray-300" href="#/">
+                     Resume<BsFillPersonLinesFill size={30}/>
+                  </a>
+               </li>
+            </ul>
+         </div>
+      </div> 
    )
 }
 
