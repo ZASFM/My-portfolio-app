e1aa7064eea43643018cabcffe240f13f5baa544
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2022-12-13 12:57:57 +0100

    Adding a work page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Skills from './components/Skills';
+import Work from './components/Work';
 
 const App=()=>{
   return (
@@ -11,6 +12,7 @@ const App=()=>{
       <Home/>
       <About/>
       <Skills/>
+      <Work/>
     </div>
   )
 } 
